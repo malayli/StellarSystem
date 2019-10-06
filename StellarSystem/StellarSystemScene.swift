523efb2fa6874076d903cdcf31c87858326ab1b9
Malik Alayli <malik.alayli@gmail.com>
2019-10-06 14:39:55 +0900

    Improvements

--- StellarSystem/StellarSystemScene.swift
+++ StellarSystem/StellarSystemScene.swift
@@ -23,72 +23,83 @@ final class StellarSystemScene: SCNScene {
         sunGroupNode.position = SCNVector3Make(0, 0, 0)
         contentNode.addChildNode(sunGroupNode)
         
-        sunGroupNode.addChildNode(sunLightNode(angle: -Float(Double.pi/2)))
-        sunGroupNode.addChildNode(sunLightNode(angle: -Float(Double.pi/4)))
-        sunGroupNode.addChildNode(sunLightNode(angle: -Float(Double.pi/8)))
-        sunGroupNode.addChildNode(sunLightNode(angle: 0))
+        sunGroupNode.addChildNode(SunLightNode(angle: -Float(Double.pi/2)))
+        sunGroupNode.addChildNode(SunLightNode(angle: -Float(Double.pi/4)))
+        sunGroupNode.addChildNode(SunLightNode(angle: -Float(Double.pi/8)))
+        sunGroupNode.addChildNode(SunLightNode(angle: 0))
         
-        let sunNode = sphereNode("sun", radius: 1.5, lightningModel: .constant, duration: 20.0)
+        let sunNode = SphereNode("sun", radius: 1.5, lightningModel: .constant, duration: 20.0)
         sunGroupNode.addChildNode(sunNode)
         
         // Mercury-group (will contain the Earth, and the Moon)
         let mercuryRotationNode = planetRotationNode()
-        let mercuryGroupNode = planetGroupNode(sphereNode: sphereNode("mercury", radius: 0.4), position: SCNVector3Make(3, 0, 0))
+        let mercuryGroupNode = planetGroupNode(sphereNode: SphereNode("mercury", radius: 0.4), position: SCNVector3Make(3, 0, 0))
         mercuryRotationNode.addChildNode(mercuryGroupNode)
         sunGroupNode.addChildNode(mercuryRotationNode)
         
         // Venus-group (will contain the Earth, and the Moon)
         let venusRotationNode = planetRotationNode(angle: Float(Double.pi/8))
-        let venusGroupNode = planetGroupNode(sphereNode: sphereNode("venus", radius: 0.9), position: SCNVector3Make(6, 0, 0))
+        let venusGroupNode = planetGroupNode(sphereNode: SphereNode("venus", radius: 0.9), position: SCNVector3Make(6, 0, 0))
         venusRotationNode.addChildNode(venusGroupNode)
         sunGroupNode.addChildNode(venusRotationNode)
         
         // Earth-group (will contain the Earth, and the Moon)
         let earthRotationNode = planetRotationNode(angle: Float(Double.pi/4))
-        let earthGroupNode = planetGroupNode(sphereNode: sphereNode("earth", castsShadow: true, radius: 1.0), position: SCNVector3Make(10, 0, 0))
-        let moonNode = satelliteNode("moon")
+        let earthGroupNode = planetGroupNode(sphereNode: SphereNode("earth", castsShadow: true, radius: 1.0), position: SCNVector3Make(10, 0, 0))
+        let moonNode = SatelliteNode("moon")
         earthGroupNode.addChildNode(satelliteRotationNode(satelliteNode: moonNode))
         earthRotationNode.addChildNode(earthGroupNode)
         sunGroupNode.addChildNode(earthRotationNode)
         
         // Mars-group (will contain the Earth, and the Moon)
         let marsRotationNode = planetRotationNode(angle: Float(Double.pi/2))
-        let marsGroupNode = planetGroupNode(sphereNode: sphereNode("mars", radius: 0.8), position: SCNVector3Make(16, 0, 0))
+        let marsGroupNode = planetGroupNode(sphereNode: SphereNode("mars", radius: 0.8), position: SCNVector3Make(16, 0, 0))
         marsRotationNode.addChildNode(marsGroupNode)
         sunGroupNode.addChildNode(marsRotationNode)
     }
 }
 
-private extension StellarSystemScene {
-    private func sunLightNode(angle: Float) -> SCNNode {
-        let sunLightNode = SCNNode()
-        sunLightNode.castsShadow = false
-        sunLightNode.light = SCNLight()
-        sunLightNode.light?.castsShadow = true
-        sunLightNode.light?.type = .spot
-        sunLightNode.light?.color = UIColor.white
-        sunLightNode.light?.spotInnerAngle = 0
-        sunLightNode.light?.spotOuterAngle = 90
-        sunLightNode.position = SCNVector3(0, 0, 0)
-        sunLightNode.orientation = SCNQuaternion(0, 0, 0, 0)
-        
-        sunLightNode.addAnimation(duration: 20.0, from: NSValue(scnVector4: SCNVector4Make(0, 1, 0, angle)), to: NSValue(scnVector4: SCNVector4Make(0, 1, 0, angle + Float(Double.pi) * 2.0)), key: "sun rotation")
-        return sunLightNode
+private final class SunLightNode: SCNNode {
+    convenience init(angle: Float) {
+        self.init()
+        castsShadow = false
+        light = SCNLight()
+        light?.castsShadow = true
+        light?.type = .spot
+        light?.color = UIColor.white
+        light?.spotInnerAngle = 0
+        light?.spotOuterAngle = 90
+        position = SCNVector3(0, 0, 0)
+        orientation = SCNQuaternion(0, 0, 0, 0)
+        addAnimation(duration: 20.0, from: NSValue(scnVector4: SCNVector4Make(0, 1, 0, angle)), to: NSValue(scnVector4: SCNVector4Make(0, 1, 0, angle + Float(Double.pi) * 2.0)), key: "sun light rotation")
     }
 }
 
-private extension StellarSystemScene {
-    private func sphereNode(_ imageName: String,
-                            castsShadow: Bool = false,
-                            radius: CGFloat,
-                            lightningModel: SCNMaterial.LightingModel = .lambert,
-                            duration: CFTimeInterval = 4.0) -> SCNNode {
-        let sphereNode = SCNNode(radius: radius, imageName: imageName)
-        sphereNode.castsShadow = castsShadow
-        sphereNode.position = SCNVector3Make(0, 0, 0)
-        sphereNode.geometry?.firstMaterial?.lightingModel = lightningModel
-        sphereNode.addAnimation(duration: duration, from: NSValue(scnVector4: SCNVector4Make(0, 1, 0, 0)), to: NSValue(scnVector4: SCNVector4Make(0, 1, 0, Float(Double.pi) * 2.0)), key: "planet \(imageName) rotation")
-        return sphereNode
+private final class SphereNode: SCNNode {
+    convenience init(_ imageName: String,
+    castsShadow: Bool = false,
+    radius: CGFloat,
+    lightningModel: SCNMaterial.LightingModel = .lambert,
+    duration: CFTimeInterval = 4.0) {
+        self.init()
+        geometry = SCNSphere(radius: radius)
+        geometry?.firstMaterial?.diffuse.contents = UIImage(named: imageName)
+        self.castsShadow = castsShadow
+        position = SCNVector3Make(0, 0, 0)
+        geometry?.firstMaterial?.lightingModel = lightningModel
+        addAnimation(duration: duration, from: NSValue(scnVector4: SCNVector4Make(0, 1, 0, 0)), to: NSValue(scnVector4: SCNVector4Make(0, 1, 0, Float(Double.pi) * 2.0)), key: "sphere \(imageName) rotation")
+    }
+}
+
+private final class SatelliteNode: SCNNode {
+    convenience init(_ imageName: String) {
+        self.init()
+        geometry = SCNSphere(radius: 0.5)
+        geometry?.firstMaterial?.diffuse.contents = UIImage(named: imageName)
+        castsShadow = true
+        position = SCNVector3Make(2, 0, 0)
+        geometry?.firstMaterial?.lightingModel = .lambert
+        addAnimation(duration: 4.0, from: NSValue(scnVector4: SCNVector4Make(0, 1, 0, 0)), to: NSValue(scnVector4: SCNVector4Make(0, 1, 0, Float(Double.pi) * 2.0)), key: "satellite rotation")
     }
 }
 
@@ -111,15 +122,6 @@ private extension StellarSystemScene {
 }
 
 private extension StellarSystemScene {
-    private func satelliteNode(_ imageName: String) -> SCNNode {
-        let satelliteNode = SCNNode(radius: 0.5, imageName: imageName)
-        satelliteNode.castsShadow = true
-        satelliteNode.position = SCNVector3Make(2, 0, 0)
-        satelliteNode.geometry?.firstMaterial?.lightingModel = .lambert
-        satelliteNode.addAnimation(duration: 4.0, from: NSValue(scnVector4: SCNVector4Make(0, 1, 0, 0)), to: NSValue(scnVector4: SCNVector4Make(0, 1, 0, Float(Double.pi) * 2.0)), key: "satellite rotation")
-        return satelliteNode
-    }
-    
     private func satelliteRotationNode(satelliteNode: SCNNode) -> SCNNode {
         // Moon-rotation (center of rotation of the Moon around the Earth)
         let satelliteRotationNode = SCNNode()
@@ -163,12 +165,6 @@ private extension StellarSystemScene {
 }
 
 private extension SCNNode {
-    convenience init(radius: CGFloat, imageName: String) {
-        self.init()
-        geometry = SCNSphere(radius: radius)
-        geometry?.firstMaterial?.diffuse.contents = UIImage(named: imageName)
-    }
-    
     func addAnimation(beginTime: CFTimeInterval = 0.0, duration: CFTimeInterval, from: NSValue, to: NSValue, key: String) {
         let animation = CABasicAnimation(keyPath: "rotation")
         animation.beginTime = beginTime
